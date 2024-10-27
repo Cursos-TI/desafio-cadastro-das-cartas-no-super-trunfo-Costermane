8e9e4891d79929df5ef055387fb2e580ab0a3df1
Costermane <leo.mariel_@hotmail.com>
2024-10-27 18:28:19 +0000

    concluído o desafio novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,6 +16,45 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+       
+       int pontos;
+       int populacao;
+       char nome [40];
+       char codigo [30];
+       float area;
+       char estado[20];
+       float PIB;
+
+       printf ("Digite o nome da cidade:   \n");
+       scanf ("%s" ,&nome );
+
+       printf ("Digite o código da carta:   \n");
+       scanf ("%s" ,&codigo );
+
+       printf ("Digite o estado:    \n");
+       scanf ("%s" ,&estado );
+
+       printf ("Digite a população:   \n");
+       scanf ("%d" ,&populacao );
+
+       printf ("Digite a área:     \n");
+       scanf ("%f",&area );
+
+       printf ("Digite o PIB:     \n");
+       scanf ("%f" ,&PIB );
+
+
+       printf ("Digite os pontos turísticos:   \n");
+       scanf ("%d" ,&pontos );
+
+       printf ("Nome da cidade: %s\n", nome);
+       printf ("Código da carta: %s\n", codigo);
+       printf ("Estado: %s\n", estado);
+       printf ("População: %D\n", populacao);
+       printf ("Área: %f\n", area);
+       printf ("PIB: %f\n", PIB);
+       printf (" Pontos Turísticos: %d\n", pontos);
+
 
     return 0;
 }
