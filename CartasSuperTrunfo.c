3c411b983ca5904bfc6d43e783dab8f51ccec001
Costermane <leo.mariel_@hotmail.com>
2024-11-05 03:13:57 +0000

    Nível Mestre Iniciado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,14 +20,14 @@ int main() {
        int pontos;
        int populacao;
        char nome [40];
-       char codigo [30];
+       char codigo [30];         
        float area;
        char estado[20];
        float PIB;
        float densidade = populacao / area;
        float pib = PIB / populacao; // PIB per capita.
+       float SuperPoder = (populacao + area + PIB + pib + densidade + pontos);
        
-
        printf ("Digite o nome da cidade:   \n");
        scanf ("%s" ,&nome );
 
@@ -56,7 +56,7 @@ int main() {
        printf ("População: %d\n", populacao);
        printf ("Área: %f\n", area);
        printf ("PIB: %f\n", PIB);
-       printf (" Pontos Turísticos: %d\n", pontos);
+       printf ("Pontos Turísticos: %d\n", pontos);
 
        //Nível Aventureiro;
 
@@ -66,7 +66,63 @@ int main() {
        densidade = (populacao / area);
        printf("Densidade Populacional: %.2f\n", densidade); // População dividída pela área da cidade.
     
-    return 0;
+      // return 0;
+
+       //Nível Mestre;
+
+       printf("***Comparação de Cartas***\n");
+       
+       printf ("Digite o nome da cidade da 1° Carta:   \n");
+       scanf ("%s" ,&nome );
+
+       printf ("Digite o código da 1° carta:   \n");
+       scanf ("%s" ,&codigo );
+
+       printf ("Digite o estado da 1° Carta:    \n");
+       scanf ("%s" ,&estado );
+
+       printf ("Digite a população da 1° Carta:   \n");
+       scanf ("%d" ,&populacao );
+
+       printf ("Digite a área da 1° Carta:     \n");
+       scanf ("%f",&area );
+
+       printf ("Digite o PIB da 1° Carta:     \n");
+       scanf ("%f" ,&PIB );
+
+       printf ("Digite os pontos turísticos da 1° Carta:   \n");
+       scanf ("%d" ,&pontos );
+
+         printf ("Digite o nome da cidade da 2° Carta:   \n");
+       scanf ("%s" ,&nome );
+
+       printf ("Digite o código da 2° carta:   \n");
+       scanf ("%s" ,&codigo );
+
+       printf ("Digite o estado da 2° Carta:    \n");
+       scanf ("%s" ,&estado );
+
+       printf ("Digite a população da 2° Carta:   \n");
+       scanf ("%d" ,&populacao );
+
+       printf ("Digite a área da 2° Carta:     \n");
+       scanf ("%f",&area );
+
+       printf ("Digite o PIB da 2° Carta:     \n");
+       scanf ("%f" ,&PIB );
+
+
+       printf ("Digite os pontos turísticos da 2° Carta:   \n");
+       scanf ("%d" ,&pontos );
+
+       printf("Primeira Carta: %f\n", SuperPoder);
+       printf("Segunda Carta:  %f\n", SuperPoder);
+       printf("Densidade Populacional: %f\n", densidade < den )
+    
+
+
+
+       return 0;
 
 
 
